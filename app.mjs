abf96aa536265234ecdb5844a2a861623644f68d
Sergey Trubkin <trubkinsa@gmail.com>
2024-05-04 14:15:38 +0300

    feat: add redirecting

--- app.mjs
+++ app.mjs
@@ -1,4 +1,5 @@
 import { createServer } from 'node:http';
+import { writeFile } from 'node:fs/promises';
 
 const hostname = 'localhost';
 const port = '3001';
@@ -6,23 +7,35 @@ const port = '3001';
 const server = createServer((req, res) => {
 	const { url } = req;
 	res.setHeader('Content-Type', 'text/html');
-	res.write('<!DOCTYPE html>\n');
-	res.write('<html>\n');
+
 
 	if (url === '/') {
+		res.write('<!DOCTYPE html>\n');
+		res.write('<html>\n');
 		res.write('<head>\n');
 		res.write('<title>Main Page</title>\n');
 		res.write('</head>\n');
 		res.write('<body>\n');
 		res.write('<h1>Main Page</h1>\n<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
-	} else {
-		res.write('<head>\n');
-		res.write('<title>Message Page</title>\n');
-		res.write('</head>\n');
-		res.write('<body>\n');
-		res.write('<h1>This is a Message Page!</h1>\n');
+		res.write('</body>\n');
+		res.write('</html>\n');
+		return res.end();
+	} 
+	if (url === '/message' && req.method === 'POST') {
+		console.log(req);
+		writeFile('message.txt', 'message');
+		res.statusCode = 302;
+		res.setHeader('Location', '/');
+		return res.end();
 	}
-	
+
+	res.write('<!DOCTYPE html>\n');
+	res.write('<html>\n');
+	res.write('<head>\n');
+	res.write('<title>Message Page</title>\n');
+	res.write('</head>\n');
+	res.write('<body>\n');
+	res.write('<h1>This is a Message Page!</h1>\n');
 	res.write('</body>\n');
 	res.write('</html>\n');
 	res.end();
