8fb17663fa651efa4579e3177fff8e3e38000040
Sergey Trubkin <trubkinsa@gmail.com>
2024-05-04 13:49:14 +0300

    feat: add Routing Request

--- app.mjs
+++ app.mjs
@@ -4,15 +4,25 @@ const hostname = 'localhost';
 const port = '3001';
 
 const server = createServer((req, res) => {
-	console.log(req.url, req.method, req.headers);
+	const { url } = req;
 	res.setHeader('Content-Type', 'text/html');
 	res.write('<!DOCTYPE html>\n');
 	res.write('<html>\n');
-	res.write('<head>\n');
-	res.write('<title>First Page</title>\n');
-	res.write('</head>\n');
-	res.write('<body>\n');
-	res.write('<h1>Hello, World!</h1>\n');
+
+	if (url === '/') {
+		res.write('<head>\n');
+		res.write('<title>Main Page</title>\n');
+		res.write('</head>\n');
+		res.write('<body>\n');
+		res.write('<h1>Main Page</h1>\n<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
+	} else {
+		res.write('<head>\n');
+		res.write('<title>Message Page</title>\n');
+		res.write('</head>\n');
+		res.write('<body>\n');
+		res.write('<h1>This is a Message Page!</h1>\n');
+	}
+	
 	res.write('</body>\n');
 	res.write('</html>\n');
 	res.end();
