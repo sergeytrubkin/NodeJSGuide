92aee393853161c70252ac66bc66b194499a9655
Sergey Trubkin <trubkinsa@gmail.com>
2024-05-04 13:07:11 +0300

    feat: add getting params from req

--- app.mjs
+++ app.mjs
@@ -4,10 +4,10 @@ const hostname = 'localhost';
 const port = '3001';
 
 const server = createServer((req, res) => {
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	console.log(req.url, req.method, req.headers);
 	res.end('Hello, World!\n');
 });
 
 server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
+	console.log(`Server running at http://${hostname}:${port}`);
 });
