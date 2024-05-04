a541aac7402b00e89d8c8f300ac91e9c82bb6db8
Sergey Trubkin <trubkinsa@gmail.com>
2024-05-04 13:26:49 +0300

    feat: add html to response

--- app.mjs
+++ app.mjs
@@ -5,7 +5,17 @@ const port = '3001';
 
 const server = createServer((req, res) => {
 	console.log(req.url, req.method, req.headers);
-	res.end('Hello, World!\n');
+	res.setHeader('Content-Type', 'text/html');
+	res.write('<!DOCTYPE html>\n');
+	res.write('<html>\n');
+	res.write('<head>\n');
+	res.write('<title>First Page</title>\n');
+	res.write('</head>\n');
+	res.write('<body>\n');
+	res.write('<h1>Hello, World!</h1>\n');
+	res.write('</body>\n');
+	res.write('</html>\n');
+	res.end();
 });
 
 server.listen(port, hostname, () => {
