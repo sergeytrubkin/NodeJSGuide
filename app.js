34eb2c6171340897816fb25f1c2075fe0dcf0021
Sergey Trubkin <trubkinsa@gmail.com>
2024-05-05 21:09:04 +0300

    feat: init project and add nodemon package

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { createServer } from 'node:http';
-import { requestHandler } from './routes.mjs';
+import { requestHandler } from './routes.js';
 
 const hostname = 'localhost';
 const port = 3000;
@@ -7,5 +7,5 @@ const port = 3000;
 const server = createServer(requestHandler);
 
 server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
+	console.log(`Server running at \x1b[36mhttp://${hostname}:${port}/\x1b[0m`);
 });
