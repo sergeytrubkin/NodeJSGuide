b2e8989248342a248689f62a141abeaaf0ee4b14
Sergey Trubkin <trubkinsa@gmail.com>
2024-05-05 18:51:03 +0300

    Practice - The Basics

--- app.mjs
+++ app.mjs
@@ -1,12 +1,11 @@
 import { createServer } from 'node:http';
-
 import { requestHandler } from './routes.mjs';
 
 const hostname = 'localhost';
-const port = '3001';
+const port = 3000;
 
 const server = createServer(requestHandler);
 
 server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}`);
+	console.log(`Server running at http://${hostname}:${port}/`);
 });
--- routes.mjs
+++ routes.mjs
@@ -1,49 +1,48 @@
-import { writeFile } from 'node:fs';
-const requestHandler = (req, res) => {
-	const { url } = req;
-	res.setHeader('Content-Type', 'text/html');
+export const requestHandler = (req, res) => {
+	const { url, method } = req;
 
 	if (url === '/') {
-		res.write('<!DOCTYPE html>\n');
-		res.write('<html>\n');
-		res.write('<head>\n');
-		res.write('<title>Main Page</title>\n');
-		res.write('</head>\n');
-		res.write('<body>\n');
-		res.write('<h1>Main Page</h1>\n<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
-		res.write('</body>\n');
-		res.write('</html>\n');
+		res.setHeader('Content-Type', 'text/html');
+		res.write('<html>');
+		res.write('<head><title>Enter User Name</title></head>');
+		res.write('<body>');
+		res.write('<h1>Hello! Welcome to my page!</h1>');
+		res.write('<form action="/create-user" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form>');
+		res.write('</body>');
+		res.write('</html>');
 		return res.end();
 	}
-	if (url === '/message' && req.method === 'POST') {
-		let body = [];
+
+	if (url === '/users') {
+		res.setHeader('Content-Type', 'text/html');
+		res.write('<html>');
+		res.write('<body>');
+		res.write('<ul>');
+		res.write('<li>User 1</li>');
+		res.write('<li>User 2</li>');
+		res.write('<li>User 3</li>');
+		res.write('</ul>');
+		res.write('</body>');
+		res.write('</html>');
+		return res.end();
+	}
+
+	if (url === '/create-user' && method === 'POST') {
+		const body = [];
 
 		req.on('data', (chunk) => {
 			body.push(chunk);
 		})
+
 		return req.on('end', () => {
-			const parsedBody = Buffer.concat(body).toString();
-			const message = parsedBody.split('=')[1];
-			
-			writeFile('message.txt', message, (err) => {
-				if (err) throw err;
-				res.statusCode = 302;
-				res.setHeader('Location', '/');
-				return res.end();
-			});
+			const formatData = Buffer.concat(body);
+			const username = formatData.toString().split('=')[1]
+
+			console.log(username);
+
+			res.statusCode = 302;
+			res.setHeader('Location', '/');
+			return res.end();
 		})
 	}
-
-	res.write('<!DOCTYPE html>\n');
-	res.write('<html>\n');
-	res.write('<head>\n');
-	res.write('<title>Message Page</title>\n');
-	res.write('</head>\n');
-	res.write('<body>\n');
-	res.write('<h1>This is a Message Page!</h1>\n');
-	res.write('</body>\n');
-	res.write('</html>\n');
-	res.end();
-}
-
-export { requestHandler };
+};
