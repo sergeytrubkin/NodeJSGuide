af62877f7d1951f7a0a16531759177d8e77fbc09
Sergey Trubkin <trubkinsa@gmail.com>
2024-05-22 22:26:41 +0300

    add 404 page

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+	res.status(404).send('<h1>Page not found!</h1>');
+})
+
 app.listen(port, hostname, () => {
 	console.log(`Server running at \x1b[36mhttp://${hostname}:${port}/\x1b[0m`);
 });
